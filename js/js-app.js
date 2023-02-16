2398a06559047d1a147e6eb6fbcbb722e8a9c3ce
Shabin-k <helloshabink@gmail.com>
2023-02-16 21:46:25 +0530

    .'...'

--- js/js-app.js
+++ js/js-app.js
@@ -226,7 +226,7 @@ var Staff = [
   'tel:+917994138307',//nisham
   'tel:+918138831899',//shabeeb
   'tel:+919048356766',//adeeb
-  'tel:+919207589667',
+  'tel:+919207589667',//Fuhad
   'tel:+917994138307',//nisham
   'tel:+918138831899',//shabeeb
   'tel:+919048356766',//adeeb
@@ -234,7 +234,7 @@ var Staff = [
   'tel:+917994138307',//nisham
   'tel:+918138831899',//shabeeb
   'tel:+919048356766',//adeeb
-  'tel:+919207589667',
+  'tel:+919207589667',//Fuhad
 ];
 
 const randomStaff = Math.floor(Math.random() * Staff.length);
